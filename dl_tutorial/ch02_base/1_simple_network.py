a69e64a54976c278999028567e91965e51a54558
Zephyr <edmendzhang214@gmail.com>
2026-06-02 15:49:51 +0800

    Ch02: Build basic neural network from scratch
    - Simple two-layer network implementation
    - Handwritten digit recognition (MNIST)
    - Batch processing for digit recognition

---
+++ dl_tutorial/ch02_base/1_simple_network.py
@@ -0,0 +1,39 @@
+import numpy as np
+from common.functions import sigmoid, identity    # 激活函数
+
+# 初始化网络（参数）
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.3], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+# 前向传播
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity(a3)
+
+    return y
+
+# 测试主流程
+# 1. 定义数据
+x = np.array([[1, 0.5], [2, 0.5], [3, 0.5]])
+
+# 2. 定义模型（神经网络）
+network = init_network()
+
+# 3. 预测（前向传播，得到结果）
+y = forward(network, x)
+
+print(y)
