ea3403b8a967bb9dec262ebe3f01599d4cdb2ebd
Zephyr <edmendzhang214@gmail.com>
2026-06-04 11:57:15 +0800

    add ipynb

--- dl/common/load_data.py
+++ dl/common/load_data.py
@@ -1,39 +1,107 @@
 import pandas as pd
 import torch
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
 
 
-def load_digital_data():
-    # 1. 读取数据
-    data = pd.read_csv('../data/train.csv')
+def load_digtial_data():
+    data = pd.read_csv("../data/train.csv")
 
-    # 2. 区分特征和标签
     x = data.drop("label", axis=1)
     y = data["label"]
 
-    # 3. 划分数据集: 训练集和测试集
-    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, random_state=42)
+    # 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-    # 4. 归一化
+    # 归一化
     scaler = MinMaxScaler()
-    train_x = scaler.fit_transform(train_x)
-    test_x = scaler.transform(test_x)
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
 
-    # print(type(train_x), type(test_x), type(train_y), type(test_y))
+    # 转成tensor统一返回
+    x_train = torch.tensor(x_train).float()
+    x_test = torch.tensor(x_test).float()
 
-    # 5. 统一转成tensor返回
-    train_x = torch.tensor(train_x).float()
-    test_x = torch.tensor(test_x).float()
+    y_train = torch.tensor(y_train.to_numpy())
+    y_test = torch.tensor(y_test.to_numpy())
+    return x_train, x_test, y_train, y_test
 
-    train_y = torch.tensor(train_y.to_numpy())
-    test_y = torch.tensor(test_y.to_numpy())
-    return train_x, test_x, train_y, test_y
+
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    return "cpu"
+
+
+def get_house_data():
+    # 1.加载数据
+    data = pd.read_csv("../data/house_prices.csv")
+    data.drop("Id", axis=1, inplace=True)
+    # 2. 区分特征和标签
+    x = data.drop("SalePrice", axis=1)
+    y = data["SalePrice"]
+
+    # 3. 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    # 4. 特征工程
+    ## 识别数字列和类别类
+    num_features = x.select_dtypes(exclude="object").columns
+    cat_features = x.select_dtypes(include="object").columns
+
+    num_pipeline = Pipeline([
+        ("impute", SimpleImputer(strategy="mean")),  # 用均值填充缺失的值
+        ("scaler", StandardScaler())
+    ])
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="constant", fill_value="NaN")),
+        ("onehot", OneHotEncoder(handle_unknown="ignore", drop="first", sparse_output=False))
+    ])
+
+    ct = ColumnTransformer([
+        ("num", num_pipeline, num_features),
+        ("cat", cat_pipeline, cat_features)
+    ])
+
+    x_train = ct.fit_transform(x_train)
+    x_test = ct.transform(x_test)
+
+    x_train = torch.tensor(x_train).float()
+    x_test = torch.tensor(x_test).float()
+
+    y_train = torch.tensor(y_train.values).float()
+    y_test = torch.tensor(y_test.values).float()
+
+    return x_train, x_test, y_train, y_test
+
+
+def get_fashion_data():
+    train_data = pd.read_csv("../data/fashion-mnist_train.csv")
+    test_data = pd.read_csv("../data/fashion-mnist_test.csv")
+
+    x_train = train_data.iloc[:, 1:]
+    x_test = test_data.iloc[:, 1:]
+
+    y_train = train_data.iloc[:, 0].values
+    y_test = test_data.iloc[:, 0].values
+
+    scaler = MinMaxScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+
+    x_train = torch.tensor(x_train).reshape(-1, 1, 28, 28).float()
+    x_test = torch.tensor(x_test).reshape(-1, 1, 28, 28).float()
+
+    y_train = torch.tensor(y_train).float()
+    y_test = torch.tensor(y_test).float()
+    return x_train, x_test, y_train, y_test
 
 
 if __name__ == '__main__':
-    train_x, test_x, train_y, test_y = load_digital_data()
-    print(train_x.shape)
-    print(train_y.shape)
-    print(test_x.shape)
-    print(test_y.shape)
+    get_fashion_data()
